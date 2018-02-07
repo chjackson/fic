425e50c79d6a9b153673cd322abbf85b315a9f39
Chris Jackson <chris.jackson@mrc-bsu.cam.ac.uk>
2018-02-07 12:36:46 +0000

    msm backward compatibility

--- R/fic.msm.R
+++ R/fic.msm.R
@@ -14,7 +14,10 @@
 ##' @export
 fic.msm <- function(wide, inds, inds0, gamma0=0, focus=NULL, focus_deriv=NULL, X=NULL, sub=NULL, ...){
     msm_fns <- list(coef = function(x)x$estimates,
-                    nobs = function(x)attr(model.frame(x), "ntrans"),
+                    nobs = function(x){
+                       mf <- model.frame(x)
+                       nrow(mf) - attr(mf, "npts") # attr(mf, "ntrans") since 
+                    },
                     vcov = function(x)x$covmat)
     fic.default(wide=wide, inds=inds, inds0=inds0, gamma0=gamma0, 
         focus=focus, focus_deriv=focus_deriv, X=X, sub=sub,
