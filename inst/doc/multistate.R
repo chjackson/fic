1f44fd65f17a12443cdc8a69c86a842dc05b5b3a
Christopher Jackson <chris.jackson@mrc-bsu.cam.ac.uk>
2019-02-21 22:01:49 +0000

    Vignette updates

--- inst/doc/multistate.R
+++ inst/doc/multistate.R
@@ -1,17 +1,17 @@
 ## ------------------------------------------------------------------------
-if (!require("msm")) stop("The `msm` package should be installed to run code in this vignette") 
-psor.q <- rbind(c(0,0.1,0,0), c(0,0,0.1,0), c(0,0,0,0.1), c(0,0,0,0))
-psor.wide.msm <- msm(state ~ months, subject=ptnum, data=psor, qmatrix = psor.q,  covariates = ~ollwsdrt+hieffusn, control=list(fnscale=1))
+if (!require("msm"))
+    stop("The `msm` package should be installed
+to run code in this vignette") 
+Qind <- rbind(c(0, 1, 0, 0),
+              c(0, 0, 1, 0),
+              c(0, 0, 0, 1),
+              c(0, 0, 0, 0))
+psor.wide.msm <- msm(state ~ months, subject=ptnum, data=psor, 
+                     qmatrix = Qind,  gen.inits=TRUE,
+                     covariates = ~ollwsdrt+hieffusn)
 psor.wide.msm
 
 ## ------------------------------------------------------------------------
-focus_tlos <- function(pars){
-    x.new <- updatepars.msm(psor.wide.msm, pars)
-    totlos.msm(x.new, covariates=0, t=10)["State 4"]
-}
-
-## ------------------------------------------------------------------------
-library(fic)
 inds <- rbind(
     c(1,1,1,0,0,0,0,0,0),
     c(1,1,1,0,0,0,0,0,1),
@@ -21,5 +21,17 @@ inds <- rbind(
     c(1,1,1,0,1,1,1,1,1),
     c(1,1,1,1,1,1,1,1,1)
 )
+
+## ------------------------------------------------------------------------
+totlos.msm(psor.wide.msm, covariates=0, t=10)
+
+## ------------------------------------------------------------------------
+focus_tlos <- function(pars){
+    x.new <- updatepars.msm(psor.wide.msm, pars)
+    totlos.msm(x.new, covariates=0, t=10)["State 4"]
+}
+
+## ------------------------------------------------------------------------
+library(fic)
 fic(wide=psor.wide.msm, inds=inds, focus=focus_tlos)
 
